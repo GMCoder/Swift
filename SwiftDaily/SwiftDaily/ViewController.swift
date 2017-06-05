6cb10d1d216adde96d48eba73b79ef71b2675bbd
gosu <gosu@gosudeMacBook-Pro.local>
2017-06-05 11:12:52 +0800

    backColor

---
+++ SwiftDaily/SwiftDaily/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftDaily
+//
+//  Created by gosu on 2017/6/5.
+//  Copyright © 2017年 gosu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
