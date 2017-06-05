97bfa2d9757bcde3f12568562ef1f2f32cfc5f4f
gosu <gosu@gosudeMacBook-Pro.local>
2017-06-05 13:12:56 +0800

    test0

--- SwiftDaily/SwiftDaily/ViewController.swift
+++ SwiftDaily/SwiftDaily/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate var label:UILabel!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.yellow
+        label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 200));
+        label.backgroundColor = UIColor.green;
+        label.text = "test0"
+        label.font = UIFont.systemFont(ofSize: 20);
+        label.textColor = UIColor.blue
+        self.view.addSubview(label);
     }
 
     override func didReceiveMemoryWarning() {
